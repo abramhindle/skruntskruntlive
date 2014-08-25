843d7b3f75ae85985a2a53d9c4e42caff06ba74f
skruntskrunt <skruntskrunt@skruntskrunt.ca>
2014-08-24 23:23:58 -0600

    fix marsyas build issue

--- builds/000marsyas.sh
+++ builds/000marsyas.sh
@@ -2,6 +2,6 @@
 cd ~/projects/marsyas/
 mkdir build
 cd build
-cmake -D MARSYAS_JACK=ON -D WITH_OSC=ON -D WITH_SWIG=ON -D PYTHON_INCLUDE_PATH=/usr/include/python2.7/ ../src/
+cmake -D MARSYAS_MIDIIO=OFF -D MARSYAS_JACK=ON -D WITH_OSC=ON -D WITH_SWIG=ON -D PYTHON_INCLUDE_PATH=/usr/include/python2.7/ ../src/
 make
 sudo make install
